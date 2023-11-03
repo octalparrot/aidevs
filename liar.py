368ce3f0800348f183011921338a885ab4be1480
Krzysztof <krzysiek@motokirc.pl>
2023-11-03 22:06:47 +0100

    dodano automatyczne generowanie pytan i odpowiedzi z uzyciem gpt4

--- liar.py
+++ liar.py
@@ -45,48 +45,54 @@ def submit_answer(token, answer):
 
 def evaluate_answer(question, answer):
     try:
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=f"Question: {question}\nAnswer: {answer}\nIs the answer correct?",
-            max_tokens=10
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"Question: {question}\nAnswer: {answer}\nIs the provided answer accurate and relevant to the question?"}
+            ]
         )
-        result = response['choices'][0]['text'].strip().upper()
+        result = response['choices'][0]['message']['content'].strip().upper()
         return "YES" if result == "YES" else "NO"
     except Exception as e:
         print(f"OpenAI error: {e}")
         return None
 
 
-# Lista pytań
-questions = [
-    "What is the capital of France?",
-    "Is the sky blue?",
-    "Do fish swim?",
-    "Is water wet?",
-    "Can birds fly?",
-    "Is fire hot?",
-    "Do dogs bark?",
-    "Is ice cold?",
-    "Can humans breathe underwater?",
-    "Is the Earth round?"
-]
+def generate_question():
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt="Generate a question:",
+            max_tokens=15,
+            temperature=0.7
+        )
+        question = response['choices'][0]['text'].strip()
+        return question
+    except Exception as e:
+        print(f"OpenAI error: {e}")
+        return None
+
 
 if __name__ == "__main__":
     token = get_token()
     if token:
         print(f"Uzyskany token: {token}")
-        # Wybierz losowe pytanie z listy
-        question = random.choice(questions)
-        print(f"Selected question: {question}")
-        question_response = send_question(token, question)
-        print("Server response:", question_response)
-        
-        server_answer = question_response.get('answer', '')
-        truth_value = evaluate_answer(question, server_answer)
-        print(f"Evaluation result: {truth_value}")
-        
-        if truth_value:
-            result = submit_answer(token, truth_value)
-            print("Server response:", result)
+        # Generuj pytanie za pomocą API OpenAI
+        question = generate_question()
+        if question:
+            print(f"Generated question: {question}")
+            question_response = send_question(token, question)
+            print("Server response:", question_response)
+            
+            server_answer = question_response.get('answer', '')
+            truth_value = evaluate_answer(question, server_answer)
+            print(f"Evaluation result: {truth_value}")
+            
+            if truth_value:
+                result = submit_answer(token, truth_value)
+                print("Server response:", result)
+            else:
+                print("Could not evaluate the answer.")
         else:
-            print("Could not evaluate the answer.")
+            print("Could not generate a question.")
