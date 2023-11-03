cfb680885878b7b69a5de503f3d80e29cf83763a
Krzysztof <krzysiek@motokirc.pl>
2023-11-03 21:47:34 +0100

    dodano openai i ewaluacje odpowiedzi do liara

--- liar.py
+++ liar.py
@@ -1,9 +1,14 @@
 import requests
+import random
+import openai
 from decouple import config
 
 # Ustawienia
 apikey = config('API_KEY')
 BASE_URL = "https://zadania.aidevs.pl"
+openai_api_key = config('OPENAI_API_KEY')
+
+openai.api_key = openai_api_key
 
 def get_token():
     response = requests.post(
@@ -38,14 +43,45 @@ def submit_answer(token, answer):
         print(f"Error: {response.status_code} - {response.text}")
         return None
 
+def evaluate_answer(question, answer):
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"Question: {question}\nAnswer: {answer}\nIs the answer correct?",
+            max_tokens=10
+        )
+        result = response['choices'][0]['text'].strip()
+        return result
+    except Exception as e:
+        print(f"OpenAI error: {e}")
+        return None
+# Lista pytań
+questions = [
+    "What is the capital of France?",
+    "Is the sky blue?",
+    "Do fish swim?",
+    "Is water wet?",
+    "Can birds fly?",
+    "Is fire hot?",
+    "Do dogs bark?",
+    "Is ice cold?",
+    "Can humans breathe underwater?",
+    "Is the Earth round?"
+]
 
 if __name__ == "__main__":
     token = get_token()
     if token:
         print(f"Uzyskany token: {token}")
-        question = input("Enter your question in English: ")
+        # Wybierz losowe pytanie z listy
+        question = random.choice(questions)
+        print(f"Selected question: {question}")
         question_response = send_question(token, question)
         print("Server response:", question_response)
+        
+        server_answer = question_response.get('answer', '')
+        evaluation = evaluate_answer(question, server_answer)
+        print(f"Evaluation result: {evaluation}")
         
         truth_value = None
         while truth_value not in ["YES", "NO"]:
